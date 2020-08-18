6ffb889de924161cda27af72b5dbe439615b8954
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-08-19 00:23:39 +0300

    Add user's service

---
+++ StructureToOrganize/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using StructureToOrganize.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StructureToOrganize.Services
+{
+    public class UserService
+    {
+        private readonly AuthenticationDBContext context;
+        internal UserService(AuthenticationDBContext context)
+        {
+            this.context = context;
+        }
+        public virtual User GetUser (int id)
+        {
+            return context.Find<User>(id);
+        }
+        public virtual void UpdateUser (User user)
+        {
+            context.Update<User>(user);
+            context.SaveChangesAsync();
+        }
+        public virtual void CreateUser(User user)
+        {
+            context.Add<User>(user);
+            context.SaveChangesAsync();
+        }
+        public virtual void DeleteUser (int id)
+        {
+            var entity = context.Find<User>(id);
+            context.Remove<User>(entity);
+            context.SaveChangesAsync();
+        }
+    }
+}
